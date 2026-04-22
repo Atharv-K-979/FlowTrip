744e73047a383e223aaca01d91579335166e3467
Atharv-K-979 <atharvkulkarni979@gmail.com>
2026-04-22 22:31:38 +0530

    changed bot settings3

---
+++ test-bot.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://xgtfvxeztqkjilcxexug.supabase.co";
+// Using the anon key from the user's project, but wait! I don't have their anon key!
+// The user has it in their Vercel config.
